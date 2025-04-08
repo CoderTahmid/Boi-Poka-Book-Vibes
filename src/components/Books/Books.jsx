9d2e97d48044aeac8d4762d788aaa49725ce3941
CoderTahmid <tahmid.ibne@gmail.com>
2025-04-08 11:52:34 +0600

    Banner done

---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const Books = () => {
+
+    const [] = useState();
+
+    return (
+        <div>
+            <h2 className="font-bold text-4xl text-center">Books</h2>
+        </div>
+    );
+};
+
+export default Books;
