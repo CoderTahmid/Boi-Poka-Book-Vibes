83da41eb82dae3ef5f2cd827e264a57b175946d1
CoderTahmid <tahmid.ibne@gmail.com>
2025-04-20 23:50:28 +0600

    books card showed with proper design

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,40 @@
+const Book = ({ book }) => {
+
+    const { image, bookName, author, tags, category } = book;
+
+    return (
+        <div className="card bg-base-100 w-96 shadow-sm p-6">
+            <figure className="bg-blue-200 py-8 rounded-2xl">
+                <img
+                    src={image}
+                    className="h-[166px]"
+                    alt={bookName} />
+            </figure>
+            <div className="card-body">
+                <div className="justify-center flex">
+                    {
+                        tags.map(tag => <button className="btn btn-sm bg-green-200 text-green-500">{tag}</button>)
+                    }
+                </div>
+                <h2 className="card-title">
+                    {bookName}
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p>By: {author}</p>
+                <div className="border-t-2 border-dashed"></div>
+                <div className="card-actions justify-between    ">
+                    <div className="badge badge-outline border-white">{category}</div>
+                    <div className="rating">
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" aria-label="1 star" />
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" aria-label="2 star" defaultChecked />
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" aria-label="3 star" />
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" aria-label="4 star" />
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" aria-label="5 star" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
 
-    const [] = useState();
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch("./booksData.json")
+            .then(res => res.json())
+            .then(data => setBooks(data));
+    }, []);
 
     return (
         <div>
             <h2 className="font-bold text-4xl text-center">Books</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    books.map(book => <Book key={book.bookId} book={book}></Book>)
+                }
+            </div>
         </div>
     );
 };
 
-export default Books;
+export default Books; 
