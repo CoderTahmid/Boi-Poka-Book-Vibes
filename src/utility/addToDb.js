29e31846c41c720f710ba81f9fd794d435271ee0
CoderTahmid <tahmid.ibne@gmail.com>
2025-04-28 16:17:07 +0600

    Toastify notification added in terms of listing books

--- src/utility/addToDb.js
+++ src/utility/addToDb.js
@@ -1,3 +1,5 @@
+import { toast } from "react-toastify";
+
 const getStoredReadlist = () => {
     const storedListStr = localStorage.getItem('read-list');
     if (storedListStr) {
@@ -17,6 +19,7 @@ const addToStoredReadList = (id) => {
         storedList.push(id);
         const storedListStr = JSON.stringify(storedList);
         localStorage.setItem("read-list", storedListStr);
+        toast("This book is added to your readlist");
     };
 };
 
